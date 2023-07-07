e187ea0df2d00b3958a33a15bc0e155922580062
ronit18 <ronitgandhi96@gmail.com>
2023-07-07 09:28:53 +0530

    Added try catch in every endpoint

--- veggie/views.py
+++ veggie/views.py
@@ -1,92 +1,129 @@
 from django.shortcuts import render, redirect
 from .models import Receipe
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 def receipe(request):
-    if request.method == "POST":
-        data = request.POST
+    try:
+        if request.method == "POST":
+            data = request.POST
 
-        recipe_name = data.get("recipe_name")
-        recipe_description = data.get("recipe_description")
-        recipe_image = request.FILES.get("recipe_image")
+            recipe_name = data.get("recipe_name")
+            recipe_description = data.get("recipe_description")
+            recipe_image = request.FILES.get("recipe_image")
 
-        Receipe.objects.create(
-            recipe_name=recipe_name,
-            recipe_description=recipe_description,
-            recipe_image=recipe_image,
-        )
-        return redirect("recipe")
+            Receipe.objects.create(
+                recipe_name=recipe_name,
+                recipe_description=recipe_description,
+                recipe_image=recipe_image,
+            )
+            return redirect("recipe")
 
-    queryset = Receipe.objects.all()
+        queryset = Receipe.objects.all()
 
-    if request.GET.get("search"):
-        print(request.GET.get("search"))
-        queryset.queryset = queryset.filter(
-            recipe_name__icontains=request.GET.get("search")
-        )
+        if request.GET.get("search"):
+            print(request.GET.get("search"))
+            queryset.queryset = queryset.filter(
+                recipe_name__icontains=request.GET.get("search")
+            )
 
-    context = {"recipes": queryset}
-    return render(request, "recipe.html", context)
+        context = {"recipes": queryset}
+        return render(request, "recipe.html", context)
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong")
 
 
 def delete_receipe(request, id):
-    queryset = Receipe.objects.get(id=id)
-    queryset.delete()
-    return redirect("recipe")
+    try:
+        queryset = Receipe.objects.get(id=id)
+        queryset.delete()
+        return redirect("recipe")
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong")
 
 
 def update_receipe(request, id):
-    queryset = Receipe.objects.get(id=id)
+    try:
+        queryset = Receipe.objects.get(id=id)
+        if request.method == "POST":
+            data = request.POST
 
-    if request.method == "POST":
-        data = request.POST
+            recipe_name = data.get("recipe_name")
+            recipe_description = data.get("recipe_description")
+            recipe_image = request.FILES.get("recipe_image")
 
-        recipe_name = data.get("recipe_name")
-        recipe_description = data.get("recipe_description")
-        recipe_image = request.FILES.get("recipe_image")
+            queryset.recipe_name = recipe_name
+            queryset.recipe_description = recipe_description
 
-        queryset.recipe_name = recipe_name
-        queryset.recipe_description = recipe_description
+            if recipe_image:
+                queryset.recipe_image = recipe_image
 
-        if recipe_image:
-            queryset.recipe_image = recipe_image
+            queryset.save()
+            return redirect("recipe")
 
-        queryset.save()
-        return redirect("recipe")
+        context = {"recipe": queryset}
 
-    context = {"recipe": queryset}
-
-    return render(request, "update_receipe.html", context)
+        return render(request, "update_receipe.html", context)
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong")
 
 
 def login_page(request):
-    return render(request, "login.html")
+    try:
+        if request.method == "POST":
+            data = request.POST
+
+            username = data.get("username")
+            password = data.get("password")
+
+            usernameDB = User.objects.filter(username=username)
+            userPassCheck = authenticate(username=username, password=password)
+            if usernameDB.exists() and userPassCheck is None:
+                messages.error(request, "Invalid username or password")
+                return redirect("login")
+
+            else:
+                login(request, userPassCheck)
+                return redirect("recipe")
+
+        return render(request, "login.html")
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong")
 
 
 def register_page(request):
-    if request.method == "POST":
-        data = request.POST
+    try:
+        if request.method == "POST":
+            data = request.POST
 
-        username = data.get("username")
-        first_name = data.get("first_name")
-        last_name = data.get("last_name")
-        password = data.get("password")
+            username = data.get("username")
+            first_name = data.get("first_name")
+            last_name = data.get("last_name")
+            password = data.get("password")
 
-        usernameDB = User.objects.filter(username=username)
-        if usernameDB.exists():
-            messages.error(request, "Username already exists")
+            usernameDB = User.objects.filter(username=username)
+            if usernameDB.exists():
+                messages.error(request, "Username already exists")
+                return redirect("register")
+
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+            )
+            messages.success(request, "Account created successfully")
+
+            user.set_password(password)
             return redirect("register")
 
-        user = User.objects.create_user(
-            first_name=first_name,
-            last_name=last_name,
-            username=username,
-        )
-        messages.success(request, "Account created successfully")
-
-        user.set_password(password)
-        return redirect("register")
-
-    return render(request, "register.html")
+        return render(request, "register.html")
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong")
