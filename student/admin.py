ab1f173e1209d255baaf2bd3194c5f0926875b69
ronit18 <ronitgandhi96@gmail.com>
2023-07-07 15:29:44 +0530

    register to admin of new tables.

--- student/admin.py
+++ student/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from student.models import Department, StudentID, Student
+from student.models import Department, StudentID, Student, Subject, SubjectMarks
 
 # Register your models here.
 admin.site.register(Department)
 admin.site.register(StudentID)
 admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(SubjectMarks)
