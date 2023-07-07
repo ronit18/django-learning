b138565189c0571575592ce8da97fc0aed330a5d
ronit18 <ronitgandhi96@gmail.com>
2023-07-07 13:20:29 +0530

    Added Faker to seed db and created a seed script to generate fake data..

---
+++ veggie/seed.py
@@ -0,0 +1,33 @@
+from faker import Faker
+from .models import Department, StudentID, Student
+
+fake = Faker()
+import random
+
+
+def seedDB(n=100) -> None:
+    try:
+        for _ in range(n):
+            departments_obj = Department.objects.all()
+            random_index = random.randint(0, len(departments_obj) - 1)
+            student_id = f"STU-{random.randint(100, 9999)}"
+
+            department = departments_obj[random_index]
+            student_name = fake.name()
+            student_email = fake.email()
+            student_age = random.randint(12, 30)
+            student_address = fake.address()
+
+            student_id_obj = StudentID.objects.create(student_id=student_id)
+
+            student_obj = Student.objects.create(
+                department=department,
+                student_id=student_id_obj,
+                student_name=student_name,
+                student_email=student_email,
+                student_age=student_age,
+                student_address=student_address,
+            )
+            student_obj.save()
+    except Exception as e:
+        print(e)
